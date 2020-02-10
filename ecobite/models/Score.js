0e6d8ee09ab7af359af13879e17062a33d5102a0
Mia Lavanti <mialavanti@Mias-MacBook-Air-3.local>
2020-02-10 15:32:05 +0100

    added models

---
+++ ecobite/models/Score.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const scoreSchema = new Schema({
+    userID: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    restaurantID: {
+        type: Schema.Types.ObjectId,
+        ref: "Restaurant",
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now()
+    },
+    scores: {
+        eatIn: Boolean,
+        takeAway: Number,
+        reusableCup: Boolean,
+        veg: Number,
+        dairy: Boolean
+    }
+
+});
+
+const Score = mongoose.model('Score', scoreSchema);
+module.exports = Score;
