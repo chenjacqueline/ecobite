a148d5e1d19510668196e0dce76aa6e6f35fedc3
Mia Lavanti <mialavanti@Mias-MacBook-Air-3.local>
2020-02-11 13:16:21 +0100

    added popups to restaurant pins in map

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require("axios");
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const endpoint = `https://api.foursquare.com/v2/venues/search?ll=52.5200,13.40508&radius=2000&limit=20&query=food&intent=browse&client_id=${clientId}&client_secret=${clientSecret}&v=20200210`;
+const endpoint = `https://api.foursquare.com/v2/venues/search?ll=52.5200,13.40508&radius=5000&limit=50&query=food&intent=browse&client_id=${clientId}&client_secret=${clientSecret}&v=20200210`;
 
 router.get("/restaurants", (req, res, next) => {
   console.log("REST");
